215633480960c338f0ad1e4c24a9e487bdbb0c4e
sigmatri <tatstrian@gmail.com>
2020-02-04 15:36:38 +0700

    50% progress

---
+++ FrontEnd/labstruktur/src/components/LabStruktur.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="logo">
+    <p>LabStruktur</p>
+    <div class="line"></div>
+  </div>
+</template>
+
+<style>
+.line{
+  width: 95px;
+  height: 6px;
+  text-align: left;
+  background-color: #1592E6;
+  line-height: 15px;
+  margin: 0 0 0 5px;
+}
+.logo{
+  font-family: "montserrat Alternates", sans-serif;
+  font-size: 45px;
+  color: #292929;
+  padding: 0 0 0 5px;
+  margin: 0;
+}
+</style>
---
+++ FrontEnd/labstruktur/src/views/Login.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="row">
+    <div class="col-sm-9"></div>
+      <div class="col-sm-3">
+          <div class="logo">
+            <LabStruktur/>
+          </div>
+          <div class="login">
+            <p>username</p>
+            <input placeholder="username" type="text"/> 
+            <p>password</p>
+            <input placeholder="password" type="password"/>
+            <p>lupa password ?</p>
+            <div class="button">
+              <a href="#haha">Login</a>
+            </div>
+          </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LabStruktur from "@/components/LabStruktur.vue";
+export default {
+  name: "logo",
+  components: {
+    LabStruktur
+  }
+};
+</script>
+
+<style>
+.logo{
+  margin: auto;
+  padding-top: 25px;
+}
+.col-sm-9{
+  background-color : #1C45A4;
+}
+.login{
+  padding-top: 25px;
+}
+.button{
+  width: 200px;
+  border-style: solid;
+  border-color: #24D39B;
+  background-color: #24D39B;
+  border-radius: 10px 0 10px 0;
+  margin:0;
+  padding:0;
+}
+.button p{
+  text-align: center;
+  overflow: hidden;
+  padding: 10px;
+}
+</style>
