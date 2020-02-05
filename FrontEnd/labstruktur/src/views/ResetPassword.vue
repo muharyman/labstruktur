7de1734ca92833528560f4a0ff7fb4b81f647aa0
sigmatri <tatstrian@gmail.com>
2020-02-06 04:00:10 +0700

    resetpassword sudah selesai && fix responsive

--- FrontEnd/labstruktur/src/views/ResetPassword.vue
+++ FrontEnd/labstruktur/src/views/ResetPassword.vue
@@ -1,19 +1,30 @@
 <template>
   <div class="root">
-    <LabStruktur/>
-      HAI SAYANG
+    <div class="container-1">
+      <div class="logo-2">
+        <LabStruktur2/>
+      </div>
+      <div class="resetpassword">
+        <p id="title-1">reset password</p>
+        <input class="txt_input-3" placeholder="new password" type="text"/>
+        <input class="txt_input-3" placeholder="confirm your password" type="text"/>
+        <div class="button-3">
+            <a href="#none">kirim</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-import LabStruktur from "@/components/LabStruktur.vue";
+import LabStruktur2 from "@/components/LabStruktur2.vue";
 export default {
   name: "logo",
   components: {
-    LabStruktur
+    LabStruktur2
   }
 }
 </script>
 
 <style>
-
+  @import '../styles/resetpassword.css';
 </style>
