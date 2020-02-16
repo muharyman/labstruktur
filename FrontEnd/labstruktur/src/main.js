1a3f02e4d4ecd2b4b6824f42db4c401b0c61e756
Kevin Muharyman <kevinmuharyman@Kevins-MacBook-Air.local>
2020-02-16 12:58:29 +0700

    list pengujian list inventaris dashboard

--- FrontEnd/labstruktur/src/main.js
+++ FrontEnd/labstruktur/src/main.js
@@ -5,23 +5,23 @@ import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 import "vuetify/dist/vuetify.min.css";
-
-
+import vuetify from "@/plugins/vuetify";
+// import VueCharts from 'vue-chartjs';
 
 Vue.config.productionTip = false;
 
-
 new Vue({
+  vuetify,
   router,
   store,
   render: h => h(App)
 }).$mount("#app");
 
 // Install BootstrapVue
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+Vue.use(IconsPlugin);
