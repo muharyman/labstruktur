67f5fc7f0b15b5c7441f8c4b8b71b2073553e63e
sigmatri <tatstrian@gmail.com>
2020-02-17 23:57:34 +0700

    benerin logo labstruktur

--- FrontEnd/labstruktur/src/components/LabStruktur.vue
+++ FrontEnd/labstruktur/src/components/LabStruktur.vue
@@ -7,7 +7,7 @@
 
 <style scoped>
 .line{
-  width: 5vw;
+  width: 100px;
   height: 2px;
   text-align: left;
   background-color: #24D39B;
@@ -15,15 +15,13 @@
   margin: 0 0 0 3px; 
 }
 .labstruktur{
-  width: fit-content;
   font-family: "montserrat Alternates", sans-serif;
   font-weight: 700;
   font-size: 26px;
   color: #292929;
   padding: 0 0 0 5px;
   margin: auto;
-  display: block;
-  display: inline-block;
   line-height: 12px;
+  width: 100%;
 }
 </style>
